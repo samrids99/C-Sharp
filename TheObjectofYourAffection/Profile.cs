81bf64af8f5dc47c52fbe1e13473a01f7edac7e6
Sam Ridsdale <samridsdale108@gmail.com>
2024-07-21 21:35:49 +0100

    fields added to profile

--- TheObjectofYourAffection/Profile.cs
+++ TheObjectofYourAffection/Profile.cs
@@ -2,4 +2,13 @@ using System;
 
 namespace DatingProfile
 {
+  public class Profile
+  {
+    string name;
+    int age;
+    string city;
+    string country;
+    string pronouns;
+    string[] hobbies;
+  }
 }
