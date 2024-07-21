54c799bb32d0cc8807023488fb58a9808600e928
Sam Ridsdale <samridsdale108@gmail.com>
2024-07-21 21:49:38 +0100

    constructor initialised

--- TheObjectofYourAffection/Profile.cs
+++ TheObjectofYourAffection/Profile.cs
@@ -4,11 +4,23 @@ namespace DatingProfile
 {
   class Profile
   {
+    // fields
     private string name;
     private int age;
     private string city;
     private string country;
     private string pronouns;
     private string[] hobbies;
+
+    // constructors
+    public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+    {
+      this.name = name;
+      this.age = age;
+      this.city = city;
+      this.country = country;
+      this.pronouns = pronouns;
+      this.hobbies = new string[] {};
+    }
   }
 }
