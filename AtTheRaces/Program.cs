21a00a101fe1779103d3a2a68bc5bcd4072fedf3
Sam Ridsdale <samridsdale108@gmail.com>
2024-07-21 22:17:35 +0100

    introductions to user

--- AtTheRaces/Program.cs
+++ AtTheRaces/Program.cs
@@ -6,6 +6,12 @@ namespace AtTheRaces
   {
     static void Main(string[] args)
     {
+      int userMoney = 150;
+      bool continue = true;
+      Random winner = new Random();
+
+      Console.WriteLine("Welcome to your day at the races!!");
+      Console.WriteLine("A fun little game to demonstrate some C# knowledge :)");
 
     }
   }
