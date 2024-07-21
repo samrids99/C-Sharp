4270d0a4ff45a6f35360fe0acc38207203619d5a
Sam Ridsdale <samridsdale108@gmail.com>
2024-07-21 22:20:57 +0100

    instructions laid out

--- AtTheRaces/Program.cs
+++ AtTheRaces/Program.cs
@@ -12,7 +12,10 @@ namespace AtTheRaces
 
       Console.WriteLine("Welcome to your day at the races!!");
       Console.WriteLine("A fun little game to demonstrate some C# knowledge :)");
-
+      Console.WriteLine("\n");
+      Console.WriteLine("6 horses are about to give it their all to reach the finish first and claim glory, your job is to pick the winner");
+      Console.WriteLine("\n");
+      Console.WriteLine("First place will see your bet tripled, whilst second and third provide your money back. Can you reach $1000..?")
     }
   }
 
