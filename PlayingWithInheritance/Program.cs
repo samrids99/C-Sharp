b5e5dcf16ac2efd716ff8cc4e9220385218f1f12
Sam Ridsdale <samridsdale108@gmail.com>
2024-07-23 23:04:14 +0100

    bugs fixing

--- PlayingWithInheritance/Program.cs
+++ PlayingWithInheritance/Program.cs
@@ -4,10 +4,23 @@ namespace PlayingWithInheritance
 {
   class Program
   {
-    public static Main(string[] args)
+   static void Main(string[] args)
     {
+
+      // generic footballer
+      Footballer sam = new Footballer("Sam Ridsdale", "England", 8);
+      Console.WriteLine(sam.Score());
+      Console.WriteLine(sam.Kick());
+
+      // reserve
       Reserve marc = new Reserve("Marc Cucurella", "España", 24);
       Console.WriteLine(marc.Score());
+      Console.WriteLine(marc.Kick());
+
+      // starter
+      Starter cold = new Starter("Cold Palmer", "England", 20);
+      Console.WriteLine(cold.Kick());
+      Console.WriteLine(cold.Win());
     }
   }
 
--- PlayingWithInheritance/Starter.cs
+++ PlayingWithInheritance/Starter.cs
@@ -7,6 +7,11 @@ namespace PlayingWithInheritance
     public Starter(string name, string nationality, int shirtNumber) : base(name, nationality, shirtNumber)
     {
     }
+
+    public string Win()
+    {
+      return $"The man hailing from {Nationality} finally lifts the trophy for his country! Remember the name...{Name}";
+    }
   }
 
 }
